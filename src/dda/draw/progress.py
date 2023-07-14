e70789aab8fce7ba426b3ae16bc502cd539c0db1
makinzm <yuruseba@yahoo.co.jp>
2023-07-14 22:14:21 +0900

    progress figure

--- src/dda/draw/progress.py
+++ src/dda/draw/progress.py
@@ -7,7 +7,9 @@ import datetime
 
 from dda.draw.aim import draw_aim
 
-def visualize_progress(start_date: datetime.date, total_pages: int, pages_per_day_goal:int, input_data: Dict[str, int], save_path: Optional[str] = None) -> Optional[tuple[ValuesView[str], ValuesView[int]]]:
+def visualize_progress(
+        start_date: datetime.date, total_pages: int, pages_per_day_goal:int, input_data: Dict[str, int], save_path: Optional[str] = None
+    ) -> Optional[tuple[tuple[ValuesView[str], ValuesView[int]], tuple[list[datetime.date], list[int]]]]:
     dates: DatetimeIndex
     pages: list[int]
     aim_dates, aim_pages = draw_aim(start_date, total_pages, pages_per_day_goal)
@@ -16,21 +18,27 @@ def visualize_progress(start_date: datetime.date, total_pages: int, pages_per_da
     for pages_day_actual in input_data.values():
         pages_sum -= pages_day_actual
         plot_summation_list.append(pages_sum)
-    fig: Figure
-    ax: Axes
-    fig, ax = plt.subplots()
-    ax.plot(aim_dates, aim_pages, 'r-', label='Aim')
-    ax.plot([datetime.datetime.strptime(_date, "%Y-%m-%d").date() for _date in input_data.keys()], plot_summation_list, 'b-', label='Progress')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Pages Read')
-    ax.set_title('Reading Progress')
-    ax.legend()
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.show()
+    date_keys_actual: list[datetime.date] = [datetime.datetime.strptime(_date, "%Y-%m-%d").date() for _date in input_data.keys()]
+    if save_path is not None:
+        fig: Figure
+        ax: Axes
+        fig, ax = plt.subplots()
+        ax.plot(aim_dates, aim_pages, 'r-', label='Aim')
+        ax.plot(date_keys_actual, plot_summation_list, 'b-', label='Progress')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Pages Read')
+        ax.set_title('Reading Progress')
+        ax.legend()
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.savefig(save_file)
+        plt.show()
+    else:
+        return (aim_dates, aim_pages), (date_keys_actual, plot_summation_list)
 
 if __name__ == "__main__":
     save_file: str = "../../../tmp_files/progress.visualize_progress.png"
+
     def _create_dummy() -> Dict[str, int]:
         input_data: Dict[str, int] = {}
 
@@ -41,8 +49,12 @@ if __name__ == "__main__":
             input_data[str(date)] = pages_read
 
         return input_data
+    
     input_data: Dict[str, int] = _create_dummy()
     print("="*10)
     print(input_data)
     print("="*10)
-    visualize_progress(datetime.date.today(), 590, 10, input_data)
+
+    _tmp = visualize_progress(datetime.date.today(), 590, 10, input_data)
+    print(_tmp)
+    visualize_progress(datetime.date.today(), 590, 10, input_data, save_file)
