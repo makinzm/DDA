b69a3bef2f0073ef06e128a1cf63892f889346be
makinzm <yuruseba@yahoo.co.jp>
2023-07-16 11:47:58 +0900

    make progressing function flexible to cumlative

--- src/dda/draw/progress.py
+++ src/dda/draw/progress.py
@@ -1,5 +1,6 @@
 from typing import Dict, ValuesView, Optional
 
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
@@ -8,16 +9,16 @@ import datetime
 from dda.draw.aim import draw_aim
 
 def visualize_progress(
-        start_date: datetime.date, total_pages: int, pages_per_day_goal:int, input_data: Dict[str, int], save_path: Optional[str] = None
+        start_date: datetime.date, total_pages: int, pages_per_day_goal:int, input_data: Dict[str, int], is_cumulative_sum:bool = False, buffer_days:int = 0, save_path: Optional[str] = None
     ) -> Optional[tuple[tuple[ValuesView[str], ValuesView[int]], tuple[list[datetime.date], list[int]]]]:
     dates: DatetimeIndex
     pages: list[int]
-    aim_dates, aim_pages = draw_aim(start_date, total_pages, pages_per_day_goal)
-    plot_summation_list: list[int] = []
-    pages_sum: int = total_pages
-    for pages_day_actual in input_data.values():
-        pages_sum -= pages_day_actual
-        plot_summation_list.append(pages_sum)
+    aim_dates, aim_pages = draw_aim(start_date, total_pages, pages_per_day_goal, buffer_days = buffer_days)
+    plot_summation_list: np.ndarray
+    if is_cumulative_sum:
+        plot_summation_list = total_pages - np.cumsum(list(input_data.values()))
+    else:
+        plot_summation_list = total_pages - np.array(list(input_data.values()))
     date_keys_actual: list[datetime.date] = [datetime.datetime.strptime(_date, "%Y-%m-%d").date() for _date in input_data.keys()]
     if save_path is not None:
         fig: Figure
@@ -40,6 +41,7 @@ def visualize_progress(
 
 if __name__ == "__main__":
     save_file: str = "../../../tmp_files/progress.visualize_progress.png"
+    save_file_not_cumulative: str = "../../../tmp_files/progress.visualize_progress.not_cumulative.png"
 
     def _create_dummy() -> Dict[str, int]:
         input_data: Dict[str, int] = {}
@@ -59,4 +61,5 @@ if __name__ == "__main__":
 
     _tmp = visualize_progress(datetime.date.today(), 590, 10, input_data)
     print(_tmp)
-    visualize_progress(datetime.date.today(), 590, 10, input_data, save_file)
+    visualize_progress(datetime.date.today(), 590, 10, input_data, buffer_days=2, is_cumulative_sum=True, save_path=save_file)
+    visualize_progress(datetime.date.today(), 590, 10, input_data, buffer_days=2, is_cumulative_sum=False, save_path=save_file)
