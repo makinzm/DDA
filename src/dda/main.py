3355fe45c5cc18a8a554a91758b0475465c7d23b
makinzm <yuruseba@yahoo.co.jp>
2023-07-22 18:25:02 +0900

    [add] config.yaml

--- src/dda/main.py
+++ src/dda/main.py
@@ -2,16 +2,19 @@ import argparse
 import os
 
 from dda.sub_main import set_aim, update_progress, draw_from_yaml
+from dda.yaml.set import load_yaml, update_yaml
 
 def main():
+    saved_config_path = os.path.join(os.path.dirname(os.path.join(__file__)),"config.yaml")
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", choices=["aim", "progress"], help="Command to execute")
+    parser.add_argument("command", choices=["aim", "progress", "get-config", "set-config"], help="Command to execute")
     parser.add_argument("-r", "--root", action="store_true", help="Use current CLI root path")
     args = parser.parse_args()
-
-    path = os.path.join(os.path.dirname(os.path.dirname(os.path.join(__file__))),"resources")
     if args.root:
         path = os.getcwd()
+    else:
+        config_data = load_yaml(saved_config_path)
+        path = config_data["path"]
     aim_path = os.path.join(path,"aim.yaml")
     progress_path = os.path.join(path, "progress.yaml")
     print(aim_path, progress_path)
@@ -21,6 +24,17 @@ def main():
     elif args.command == "progress":
         update_progress(progress_path)
         draw_from_yaml(aim_path, progress_path, path)
+    elif args.command == "get-config":
+        config_data = load_yaml(saved_config_path)
+        print(config_data)
+    elif args.command == "set-config":
+        config_data = load_yaml(saved_config_path)
+        print("Current : ", config_data)
+        new_path = input("Please Input New Resource Path : ")
+        new_config = {"path": new_path}
+        update_yaml(saved_config_path, new_config)
+        config_data = load_yaml(saved_config_path)
+        print("Completed : ", config_data)
     else:
         print("Invalid command")
 
